421adcc30143ce23ac888148bca2d09a3206952d
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-05-29 19:10:56 +0600

    make admin done

--- index.js
+++ index.js
@@ -52,6 +52,38 @@ async function run() {
       const users = await cursor.toArray()
       res.send(users)
     })
+
+    app.put("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+       const filter = { email: email };
+       const updateDoc = {
+        $set: {role:"admin"},
+      };
+      
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc
+        
+      );
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
